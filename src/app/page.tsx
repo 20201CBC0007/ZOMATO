e5c934cb8e6cfc99486820ce9c4c48b41d974df5
20201CBC0007 <156300762+20201CBC0007@users.noreply.github.com>
2024-02-24 01:29:31 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/Component/Hero";
+import Carousel from "@/Component/Carousel";
+
+import HotelCards from "@/Component/HotelCards";
+
+export default function Home() {
+  return (
+    <div className=" bg-yellow-50">
+      <Hero />
+      <Carousel />
+      <HotelCards />
+    </div>
+  );
+}
